f0f46550df9dbd0973be0bf5dbbabffe6b077c6a
liangbo <liangbo01@cpirhzl.com>
2024-07-23 21:46:37 +0800

    up:升级框架

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.8.9.release"
+	Version = "v1.9.0.release"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
