1e19002d341d59d87af80a3a430cf210e56e54c6
liangbo <liangbo01@cpirhzl.com>
2024-08-08 09:49:30 +0800

    up:支持依赖注解获取

--- pkg/datasources/mysql/init.go
+++ pkg/datasources/mysql/init.go
@@ -4,13 +4,14 @@ import (
 	"github.com/liangboceo/dependencyinjection"
 	"github.com/liangboceo/yuanboot/abstractions"
 	"github.com/liangboceo/yuanboot/abstractions/health"
+	"gorm.io/gorm"
 )
 
 func init() {
 	abstractions.RegisterConfigurationProcessor(
 		func(config abstractions.IConfiguration, serviceCollection *dependencyinjection.ServiceCollection) {
 			serviceCollection.AddSingletonByImplementsAndName("mysql-master", NewMysqlDataSource, new(abstractions.IDataSource))
-			serviceCollection.AddSingleton(NewGormDb)
+			serviceCollection.AddSingletonByImplements(NewGormDb, new(gorm.DB))
 			serviceCollection.AddTransientByImplements(NewMysqlHealthIndicator, new(health.Indicator))
 		})
 
